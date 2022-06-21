78579e421faa975cda019e362abb556da431cac6
Abdulmalik <abdulmalikadegoke@gmail.com>
2022-06-21 07:16:19 +0100

    added the functions for the front end operations to the external script

---
+++ functionalities/functionalities.js
@@ -0,0 +1,16 @@
+//
+
+
+// parsing and pre processing the data into
+function preprocessData(){
+
+}
+// parsing the preprocessed data onto the front end
+function displayData() {
+
+}
+
+// parsing the preprocessed data onto the front end
+function searchForData() {
+  
+}
